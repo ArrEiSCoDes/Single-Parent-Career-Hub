6724c910c633b9850d0d0a64b639f1564ce5b9ce
Sierra covington <sierrainc96@outlook.com>
2026-06-30 11:55:00 -0400

    Add JavaScript functionality

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Welcome message in the browser console
+console.log("Welcome to the Single Parent Career Hub!");
+
+// Function to welcome visitors
+function welcomeMessage() {
+    alert("Welcome to the Single Parent Career Hub! We're glad you're here. This project is being built to help single parents find free training, better careers, resume assistance, and interview resources.");
+}
